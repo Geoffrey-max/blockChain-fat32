0d548c6910b3f901c6caeb22f2524323aebd3b2c
Geoffrey_max <geoffrey.clermont@ynov.com>
2020-01-28 10:20:17 +0100

    add block in start

--- Peer/block.js
+++ Peer/block.js
@@ -1,12 +1,23 @@
-var {stringifyAndHashElement} =require('../Tool/utils');
+var { stringifyAndHashElement } = require('../Tool/utils');
 
 class Block {
-    constructor(id, hashBackBlock, fivePages, idCont, inf) {
+    constructor(id, hashBackBlock, fivePages, idCont, inf, nonce) {
         this.id = id; //Numéros du bloc courant
         this.hashBackBlock = hashBackBlock; //Sha256 du block précédent
         this.fivePages = fivePages; //5 page précéde de leur numéro
         this.idCont = idCont; // id contributeur donnée par le peer 0
         this.inf = inf; // INFO Supplementaire
+        this.nonce = nonce;
+
+        let blockJson = {
+            id: this.id,
+            hashBackBlock: this.hashBackBlock,
+            fivePages: this.fivePages,
+            idCont: this.idCont,
+            inf: this.inf,
+            nonce: this.nonce
+        }
+
         this.hashthisBlock = stringifyAndHashElement(blockJson); //hashage du block
     }
 }
--- server.js
+++ server.js
@@ -4,31 +4,47 @@ const express = require("express");
 const app = express();
 const { getPages } = require("./Tool/utils");
 const Node = require("./Peer/node");
+const Block = require("./Peer/block");
 
 let node = new Node(argv.port);
 console.log(node);
 
 app.use(bodyParser.json());
 app.post("/node/resolve", (req, res) => {
-  let { start, end } = req.body;
-  timeout = 10; //30000* Math.random();
-  setTimeout(() => {
+    let { start, end } = req.body;
+    let blockChain = [];
+    let id = 0;
+    let hashLastBlock = null;
     let pageData = getPages(start, end);
+    let nonce = 1;
     console.log("My Page " + pageData);
+    blockChain.forEach(block_tst => {
+        if (block_tst.id === (id - 1)) {
+            hashLastBlock = block_tst.hashBackBlock;
+        }
+    });
+    if (id === 0) {
+        let block = new Block(id, null, pageData, node.id, null, nonce);
+        if (block.hashthisBlock.substr(0) === 0) {
+            blockChain.push(block);
+            id++;
+        } else {
+            nonce++;
+        }
+    }
     res.send();
-  }, timeout);
 });
 
 if (argv.port && argv.port != 5000) {
-  if (node.connection.length >0) {
-    // Start server
-    app.listen(argv.port, () => {
-      console.log(`Server listening`, argv.port);
-    });
-  } else {
-    console.log("Error : Node not create");
-  }
+    if (node.connection.length > 0) {
+        // Start server
+        app.listen(argv.port, () => {
+            console.log(`Server listening`, argv.port);
+        });
+    } else {
+        console.log("Error : Node not create");
+    }
 } else {
-  console.log("node 'main.js' --port=XXXX");
+    console.log("node 'main.js' --port=XXXX");
 }
 
